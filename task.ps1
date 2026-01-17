3da26326f9139299e61fabe929bc70f58fc46f24
Gosxa <ihorkondrashov@gmail.com>
2026-01-17 23:31:51 +0200

    task8

--- task.ps1
+++ task.ps1
@@ -1,1 +1,16 @@
-# Write your code here
+$unattechedDisks = @()
+
+Get-AzDisk | ForEach-Object {
+    $disk = $_
+    $state = $disk.DiskState
+
+    if ($state -eq 'Unattached') {
+        $unattechedDisks += $disk
+        Write-Output "Found unattached disk: $($disk.Name)"
+    }
+    else {
+        Write-Output "Skipping disk: $($disk.Name) (State: $state)"
+    }
+}
+
+$unattechedDisks | ConvertTo-Json | Out-File ./result.json
