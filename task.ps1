f90d1b996674bb38a4d008053c3a591fb7c15310
Gosxa <ihorkondrashov@gmail.com>
2026-01-18 00:00:36 +0200

    task8.1

--- task.ps1
+++ task.ps1
@@ -3,8 +3,9 @@ $unattechedDisks = @()
 Get-AzDisk | ForEach-Object {
     $disk = $_
     $state = $disk.DiskState
+    $diskgroup = $disk.ResourceGroupName
 
-    if ($state -eq 'Unattached') {
+    if ($state -eq 'Unattached' -and $diskgroup -eq 'mate-azure-task-5') {
         $unattechedDisks += $disk
         Write-Output "Found unattached disk: $($disk.Name)"
     }
